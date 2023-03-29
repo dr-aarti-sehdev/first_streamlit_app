5c0a64b80baed010de009535b25f8be2a4b6fae9
dr-aarti-sehdev <128491961+dr-aarti-sehdev@users.noreply.github.com>
2023-03-29 09:58:11 +0100

    Update streamlit_app.py
    Added Breakfast Menu with three items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Tofu scramble")
+streamlit.text("Onion and chilli omlette")
+streamlit.text("Supergreen smoothie")
