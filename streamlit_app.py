fed8ad6a4d4beca6e83b59a17a58a41db61cf0c9
dr-aarti-sehdev <128491961+dr-aarti-sehdev@users.noreply.github.com>
2023-03-29 10:02:27 +0100

    Update streamlit_app.py
    Deleted some Breakfast menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.text("Tofu scramble")
-streamlit.text("Onion and chilli omlette")
 streamlit.text("Hard-boiled free range egg")
 streamlit.text("Kale, spinach and rocket smoothie")
 streamlit.text("Omega 3 and blueberry oatmeal")
