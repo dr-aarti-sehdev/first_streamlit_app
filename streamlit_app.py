78898f804fc39d81a37b9e64fac50e2358d68c4e
dr-aarti-sehdev <128491961+dr-aarti-sehdev@users.noreply.github.com>
2023-03-29 10:05:38 +0100

    Update streamlit_app.py
    changed breakfast menu title and added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.text("Hard-boiled free range egg")
-streamlit.text("Kale, spinach and rocket smoothie")
-streamlit.text("Omega 3 and blueberry oatmeal")
+streamlit.text("🐔 Hard-boiled free range egg")
+streamlit.text("🥗 Kale, spinach and rocket smoothie")
+streamlit.text("🥣 Omega 3 and blueberry oatmeal")
