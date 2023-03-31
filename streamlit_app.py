57f64dcac839f65e57b6967637647e69a40d1e06
dr-aarti-sehdev <128491961+dr-aarti-sehdev@users.noreply.github.com>
2023-03-31 09:09:55 +0100

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,5 @@ streamlit.dataframe(fruits_to_show)
 ## call fruityvice API from app
 
 import requests
-frutyvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
