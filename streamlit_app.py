12d0cf4cf287c172390f256f0d1adbebcaa0cc01
dr-aarti-sehdev <128491961+dr-aarti-sehdev@users.noreply.github.com>
2023-03-31 21:36:47 +0100

    Update streamlit_app.py
    added all rows to snowflake output

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,8 +52,8 @@ my_cur = my_cnx.cursor()
 
 ## run sql queries!
 my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains: ")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 ## query data
